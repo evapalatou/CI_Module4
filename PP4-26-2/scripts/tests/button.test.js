59d0a7529ba53a4b43dc168016aa5306fcfeca33
Evanthia <epalatou@outlook.com>
2026-04-08 21:03:16 +0100

    more jest tests

--- PP4-26-2/scripts/tests/button.test.js
+++ PP4-26-2/scripts/tests/button.test.js
@@ -7,9 +7,7 @@ const buttonClick = require("../button");
 beforeEach(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
+    document.body.innerHTML = fileContents;
 });
 
 describe("DOM tests", () => {
